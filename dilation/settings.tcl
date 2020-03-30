7ddd56a15fc2a713413a9319297fc7e229cb7f48
obikata <yohei.sugimoto@hotmail.co.jp>
2020-03-30 14:39:47 +0900

    Initial commit

---
+++ dilation/settings.tcl
@@ -0,0 +1,8 @@
+set XPART xc7z020clg400-1
+set CSIM 1
+set CSYNTH 1
+set COSIM 0
+set VIVADO_SYN 0
+set VIVADO_IMPL 0
+set QOR_CHECK 0
+set XF_PROJ_ROOT "/home/razer/workspace/2019.2/accel-vision-pkg/"
